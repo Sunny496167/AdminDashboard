0e0af5c5223a708d69d7964793d9d6a9e48ea2f9
Sunny Kumar <sunnykumar49617@gmail.com>
2024-10-16 20:04:35 +0530

    complete 4 apps

--- src/App.jsx
+++ src/App.jsx
@@ -53,9 +53,9 @@ const App = () => {
               <Route path='/employees' element={<Employees />} />
               <Route path='/customers' element={<Customers />} />
               {/* Apps */}
+              <Route path='/calendar' element={<Calendar />} />
               <Route path='/kanban' element={<Kanban />} />
               <Route path='/editor' element={<Editor />} />
-              <Route path='/calender' element={<Calendar />} />
               <Route path='/color-picker' element={<ColorPicker />} />
               {/* Charts */}
               <Route path='/line' element={<Line />} />
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Header } from '../components'
+import { ColumnDirective, ColumnsDirective, KanbanComponent } from '@syncfusion/ej2-react-kanban'
+import { kanbanData, kanbanGrid } from '../data/dummy'
 
 const Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className='m-2 md:m-10 md:p-10 p-2 bg-white rounded-3xl'>
+      <Header category='App' title='Kanban' />
+      <KanbanComponent 
+        id='kanban'
+        dataSource={kanbanData}
+        cardSettings={{contentField: 'Summary', headerField: 'Id'}}
+        keyField='Status'
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => 
+          <ColumnDirective key={index} {...item} />
+          )}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 
